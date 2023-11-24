c45b2faf8e530b15b89d745ffb8c5a475ad8eae0
Egor Derevyanko <derevyanko.egor2705@gmail.com>
2023-11-24 16:30:37 +0300

    Первый тест

--- keyboards.py
+++ keyboards.py
@@ -2,6 +2,6 @@ from aiogram.utils.keyboard import InlineKeyboardBuilder, ReplyKeyboardBuilder
 from aiogram.types import KeyboardButton, InlineKeyboardButton, WebAppInfo, ReplyKeyboardMarkup
 
 builder = ReplyKeyboardBuilder()
-form_button = KeyboardButton(text='Перейти в форму', web_app=WebAppInfo(url='https://yandex.ru/'))
+form_button = KeyboardButton(text='Перейти в форму', web_app=WebAppInfo(url='https://legendgaming0305.github.io/Sechenov_tech/'))
 builder.add(form_button)
 markup = ReplyKeyboardMarkup(keyboard=builder.export(), resize_keyboard=True)
