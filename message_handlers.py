2357db2e6ea08631dbb1c34283a5aa45fd686246
Egor Derevyanko <derevyanko.egor2705@gmail.com>
2023-11-26 23:06:13 +0300

    test

--- message_handlers.py
+++ message_handlers.py
@@ -1,6 +1,6 @@
 from aiogram import Router, F, types
 from aiogram.filters import Command
-from aiogram.types import MenuButtonWebApp, WebAppInfo
+from aiogram.types import MenuButtonWebApp, WebAppInfo, WebAppData
 
 from keyboards import markup
 
@@ -10,6 +10,6 @@ router = Router()
 async def process_start(message: types.Message):
     await message.answer(text='Пройдите форму', reply_markup=markup)
 
-@router.message(F.web_app_data)
+@router.message()
 async def process_web_app_data(message: types.Message):
     await message.answer('Информация получена')
